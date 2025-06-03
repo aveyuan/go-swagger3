b4163f0bb36fe201eacb96b57634a74ff2f219d2
Văn Hải <haidv@interdogmedia.com>
2025-06-03 10:36:39 +0700

    add unit test

---
+++ parser/operations/parser_mock_test.go
@@ -0,0 +1,30 @@
+package operations
+
+import (
+	"go/ast"
+
+	"github.com/parvez3019/go-swagger3/openApi3Schema"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockSchemaParser struct {
+	mock.Mock
+}
+
+func (m *MockSchemaParser) GetPkgAst(pkgPath string) (map[string]*ast.Package, error) {
+	args := m.Called(pkgPath)
+	return args.Get(0).(map[string]*ast.Package), args.Error(1)
+}
+
+func (m *MockSchemaParser) RegisterType(pkgPath, pkgName, typeName string) (string, error) {
+	args := m.Called(pkgPath, pkgName, typeName)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockSchemaParser) ParseSchemaObject(pkgPath, pkgName, typeName string) (*openApi3Schema.SchemaObject, error) {
+	args := m.Called(pkgPath, pkgName, typeName)
+	if obj := args.Get(0); obj != nil {
+		return obj.(*openApi3Schema.SchemaObject), args.Error(1)
+	}
+	return nil, args.Error(1)
+}
--- parser/operations/parser_test.go
+++ parser/operations/parser_test.go
@@ -2,10 +2,11 @@ package operations
 
 import (
 	"errors"
+	"testing"
+
 	oas "github.com/parvez3019/go-swagger3/openApi3Schema"
 	"github.com/parvez3019/go-swagger3/parser/schema"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func Test_ParseHeader(t *testing.T) {
@@ -50,5 +51,72 @@ func Test_ParseHeader(t *testing.T) {
 			}
 			assert.Equal(t, test.expectedParameters, operationObject.Parameters)
 		})
+	}
+}
+
+func TestParseParamComment_FormParam(t *testing.T) {
+	p := &parser{}
+	op := &oas.OperationObject{}
+
+	comment := "@Param file form ignored true \"Upload file\" \"/path/to/file\""
+
+	err := p.parseParamComment("example/pkg", "pkg", op, comment)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+	if op.RequestBody == nil {
+		t.Error("Expected RequestBody to be set for form param")
+	}
+}
+
+func TestParseParamComment_BodyParam(t *testing.T) {
+	mockSchema := new(MockSchemaParser)
+
+	p := &parser{
+		Parser: mockSchema,
+	}
+
+	op := &oas.OperationObject{}
+
+	comment := "@Param user body User true \"User info\""
+
+	mockSchema.On("RegisterType", "example/pkg", "pkg", "User").Return("UserSchemaRef", nil)
+
+	err := p.parseParamComment("example/pkg", "pkg", op, comment)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if op.RequestBody == nil {
+		t.Error("Expected RequestBody to be set for body param")
+	}
+
+	mockSchema.AssertExpectations(t)
+}
+
+func TestParseParamComment_QueryParam(t *testing.T) {
+	p := &parser{}
+	op := &oas.OperationObject{}
+
+	comment := "@Param id query int true \"User ID\""
+
+	err := p.parseParamComment("example/pkg", "pkg", op, comment)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+	if len(op.Parameters) == 0 {
+		t.Error("Expected query param to be added to operation.Parameters")
+	}
+}
+
+func TestParseParamComment_InvalidComment(t *testing.T) {
+	p := &parser{}
+	op := &oas.OperationObject{}
+
+	comment := "@Param invalid format only"
+
+	err := p.parseParamComment("pkg", "pkg", op, comment)
+	if err == nil {
+		t.Error("Expected error for invalid comment format, but got none")
 	}
 }
